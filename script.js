9de4ab569c75eed2717b515a3c3d8b8df5b43a23
Azat <azt001@mail.ru>
2025-01-23 18:49:21 +0300

    Геймифицированный баннер

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const dragover = (event) => false;
 const moveElement = function (event, item, offsetX, offsetY) {
   //event.preventDefault();
   let touch = event.targetTouches[0];
-  item.style.position = "fixed";
+  //item.style.position = "fixed";
   item.style.left = (touch.clientX - offsetX) + "px";
   item.style.top = (touch.clientY - offsetY) + "px";
 };
@@ -42,7 +42,7 @@ const stopMoving = function (item) {
   item.removeEventListener("touchend", stopMoving);
 };
 const touchStart = function (event, item) {
-  item.style.position = "fixed";
+  //item.style.position = "fixed";
   let touch = event.targetTouches[0];
   let elemX = item.getBoundingClientRect().x;
   let elemY = item.getBoundingClientRect().y;
